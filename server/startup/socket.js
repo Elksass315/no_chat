cb3a860a4ed6d716c5dd7936fc0fd5cc38cbdb81
Elksass315 <qsasomar@gmail.com>
2024-09-09 00:51:05 +0300

    persistent user ID

--- server/startup/socket.js
+++ server/startup/socket.js
@@ -1,4 +1,5 @@
 const { Server } = require('socket.io');
+const crypto = require("crypto");
 
 module.exports = function (server) {
 
@@ -8,39 +9,92 @@ module.exports = function (server) {
         },
     });
 
+
+    
+    const randomId = () => crypto.randomBytes(8).toString("hex");
+
+    const { InMemorySessionStore } = require("../helper/sessionStore");
+    const sessionStore = new InMemorySessionStore();
+
     io.use((socket, next) => {
+        const sessionID = socket.handshake.auth.sessionID;
+        if (sessionID) {
+            const session = sessionStore.findSession(sessionID);
+            if (session) {
+                socket.sessionID = sessionID;
+                socket.userID = session.userID;
+                socket.username = session.username;
+                return next();
+            }
+        }
         const username = socket.handshake.auth.username;
-        if (!username || username.length > 20) {
+        if (!username) {
             return next(new Error("invalid username"));
         }
+        socket.sessionID = randomId();
+        socket.userID = randomId();
         socket.username = username;
         next();
     });
 
     io.on("connection", (socket) => {
+        // persist session
+        sessionStore.saveSession(socket.sessionID, {
+            userID: socket.userID,
+            username: socket.username,
+            connected: true,
+        });
+
+        // emit session details
+        socket.emit("session", {
+            sessionID: socket.sessionID,
+            userID: socket.userID,
+        });
+
+        // join the "userID" room
+        socket.join(socket.userID);
+
+        // fetch existing users
         const users = [];
-        for (let [id, socket] of io.of("/").sockets) {
+        sessionStore.findAllSessions().forEach((session) => {
             users.push({
-                userID: id,
-                username: socket.username,
+                userID: session.userID,
+                username: session.username,
+                connected: session.connected,
             });
-        }
+        });
         socket.emit("users", users);
 
+        // notify existing users
         socket.broadcast.emit("user connected", {
-            userID: socket.id,
+            userID: socket.userID,
             username: socket.username,
+            connected: true,
         });
 
+        // forward the private message to the right recipient (and to other tabs of the sender)
         socket.on("private message", ({ content, to }) => {
-            socket.to(to).emit("private message", {
+            socket.to(to).to(socket.userID).emit("private message", {
                 content,
-                from: socket.id,
+                from: socket.userID,
+                to,
             });
         });
 
-        socket.on("disconnect", () => {
-            socket.broadcast.emit("user disconnected", socket.id);
+        // notify users upon disconnection
+        socket.on("disconnect", async () => {
+            const matchingSockets = await io.in(socket.userID).allSockets();
+            const isDisconnected = matchingSockets.size === 0;
+            if (isDisconnected) {
+                // notify other users
+                socket.broadcast.emit("user disconnected", socket.userID);
+                // update the connection status of the session
+                sessionStore.saveSession(socket.sessionID, {
+                    userID: socket.userID,
+                    username: socket.username,
+                    connected: false,
+                });
+            }
         });
     });
-};
+}
